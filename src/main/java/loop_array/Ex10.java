665139adf88919c85b54be4a03f0458e34547804
Hovhannes.Gharayan <hovhannes.ghareayan@acba.am>
2024-09-18 11:53:21 +0400

    added formatting

--- src/main/java/loop_array/Ex10.java
+++ src/main/java/loop_array/Ex10.java
@@ -3,19 +3,19 @@ package loop_array;
 public class Ex10 {
 
 
-    public static void fibonacchiSequence(int n){
+    public static void fibonacchiSequence(int n) {
         int[] sequence = new int[n];
-        int current= 0;
+        int current = 0;
         int next = 1;
         int result = 0;
         for (int i = 0; i < n; i++) {
             sequence[i] = result;
-            result = current+next;
+            result = current + next;
             current = next;
             next = result;
         }
-        for (int a:sequence
-             ) {
+        for (int a : sequence
+        ) {
             System.out.println(+a);
         }
     }
